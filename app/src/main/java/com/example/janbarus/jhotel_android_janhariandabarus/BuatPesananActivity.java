9fb8a522cbfee8615a7000f8f14ab4fc116c628d
Jan Harianda Barus <jan.harianda@ui.ac.id>
2018-05-05 18:28:45 +0700

    CS_Modul9

---
+++ app/src/main/java/com/example/janbarus/jhotel_android_janhariandabarus/BuatPesananActivity.java
@@ -0,0 +1,11 @@
+package com.example.janbarus.jhotel_android_janhariandabarus;
+
+public class BuatPesananActivity {
+
+    private int CurrentUserId;
+    private int banyakHari;
+    private int idHotel;
+    private int double tariff;
+    private String roomNumber;
+
+}
